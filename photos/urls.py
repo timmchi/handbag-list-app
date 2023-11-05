091b6ec6ce303a4daf1be7a85d746befe10329f9
Timmchi <tnmagg@gmail.com>
2023-11-05 19:35:58 +0200

    Removed container and div row from the list view for a more consistent feel. Cleaned up the urls.py from unused functions.

--- photos/urls.py
+++ photos/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.gallery, name='gallery'),
     path('', views.HandbagListView.as_view(), name='gallery'),
-    # path('photo/<str:pk>', views.viewPhoto, name='photo'),
     path('photo/<str:pk>', views.HandbagDetailView.as_view(), name='photo'),
-    # path('add/', views.addPhoto, name='add'),
     path('add/', views.HandbagCreate.as_view(), name='add'),
 ]
